a6c07af5054f5c36e65b99341f38ff1a65477b28
Azamat Duisheev <azamat27071998@gmail.com>
2021-04-26 16:12:12 +0600

    finish parse

--- src/megacom/Main.java
+++ src/megacom/Main.java
@@ -13,19 +13,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Document doc = Jsoup.connect("https://www.nbkr.kg/ ").get();
-        String words = doc.title();
-        System.out.println(words);
-
-
-        //System.out.println(doc);
-
-        /*
         Elements h1Elements = doc.getElementsByAttributeValue("class", "name");
-        Elements h2Elements = doc.getAllElements();
+        System.out.println(h1Elements.text());
 
-        System.out.println(h1Elements);
-        System.out.println(h2Elements);
-*/
+//        Elements h2Elements = doc.getElementsContainingOwnText("Кыргыз Республикасынын Улуттук банкы");
+//        System.out.println(h2Elements);
+//        System.out.println(h2Elements.select("h3[class=name]").text());
     }
 }
 
